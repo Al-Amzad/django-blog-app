2c2e0110fceddeba88009942358e304d38090d97
Al-Amzad <alamzad001@outlook.com>
2021-06-27 16:57:26 +0600

    upload auth-app

--- musician/musician_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from musician_app.models import Musician, Album
-
-# Register your models here.
-admin.site.register(Musician)
-admin.site.register(Album)
--- musician/musician_app/templatetags/my_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library ( )
-
-def my_filter ( value, arg ) :
-  return value + " " + arg
-
-
-register.filter ( 'custom_filter', my_filter )
